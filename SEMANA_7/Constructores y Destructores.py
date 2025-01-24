83571620de4115f2626767c24d9937314a5c7e1f
Julia Tipan <jd.tipans@uea.edu.ec>
2025-01-23 22:53:43 -0500

    Tarea

---
+++ SEMANA_7/Constructores y Destructores.py
@@ -0,0 +1,26 @@
+class Libro:
+    # Constructor: Inicializa los atributos del objeto
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+        print(f"El libro '{self.titulo}' de {self.autor} ha sido creado.")
+
+    # Metodo para mostrar información del libro
+    def mostrar_informacion(self):
+        print(f"Título: {self.titulo}, Autor: {self.autor}")
+
+    # Destructor: Limpia recursos o realiza tareas al eliminar el objeto
+    def __del__(self):
+        print(f"El libro '{self.titulo}' de {self.autor} está siendo eliminado.")
+
+# Programa principal
+if __name__ == "__main__":
+    # Crear una instancia de la clase Libro
+    mi_libro = Libro("Crimen y castigo", "Fiódor Dostoyesvki")
+    # Llamar a un metodo de la clase
+    mi_libro.mostrar_informacion()
+    # Eliminar explícitamente el objeto
+    del mi_libro
+
+    print("Fin del programa.")
+
